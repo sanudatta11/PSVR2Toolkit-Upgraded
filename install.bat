292b1223f6b772fa1d674e334ace41fadb087c0f
Soumyajit Dutta <sdutta317@gatech.edu>
2026-03-17 01:00:53 +0530

    debug

--- install.bat
+++ install.bat
@@ -59,7 +59,7 @@ echo Starting installation...
 echo.
 
 REM Run the PowerShell installation script
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "scripts\driver-install-full.ps1" -SourceDll "%DLL_PATH%"
+powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0scripts\driver-install-full.ps1' -SourceDll '%DLL_PATH%'"
 
 if %errorLevel% equ 0 (
     echo.
